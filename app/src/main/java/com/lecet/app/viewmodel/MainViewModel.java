14a39c497005472a82359181574fd979d67f13b4
Dushan Perera <dushan@domandtom.com>
2016-10-10 12:43:05 -0400

    Added a MainViewModel which MainActivity will incorporate.

---
+++ app/src/main/java/com/lecet/app/viewmodel/MainViewModel.java
@@ -0,0 +1,60 @@
+package com.lecet.app.viewmodel;
+
+import com.lecet.app.data.models.Bid;
+import com.lecet.app.data.models.Project;
+import com.lecet.app.domain.BidDomain;
+import com.lecet.app.domain.ProjectDomain;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import io.realm.RealmResults;
+import retrofit2.Callback;
+
+/**
+ * File: MainViewModel Created: 10/6/16 Author: domandtom
+ *
+ * This code is copyright (c) 2016 Dom & Tom Inc.
+ */
+
+public class MainViewModel {
+
+    private final BidDomain bidDomain;
+    private final ProjectDomain projectDomain;
+    private final Calendar calendar;
+
+    public MainViewModel(BidDomain bidDomain, ProjectDomain projectDomain, Calendar calendar) {
+
+        this.bidDomain = bidDomain;
+        this.projectDomain = projectDomain;
+        this.calendar = calendar;
+    }
+
+    /** API **/
+
+    public void getBidsRecentlyMade(Callback<List<Bid>> callback) {
+
+        bidDomain.getBidsRecentlyMade(callback);
+    }
+
+    public void getProjectsHappeningSoon(Callback<List<Project>> callback) {
+
+        projectDomain.getProjectsHappeningSoon(callback);
+    }
+
+    /** Persisted **/
+
+    public RealmResults<Bid> fetchBids(@BidDomain.BidGroup int bidGroup) {
+
+        return bidDomain.fetchBids(bidGroup);
+    }
+
+    public RealmResults<Project> fetchProjectsHappeningSoon() {
+
+        calendar.setTime(new Date());
+        calendar.
+
+        return projectDomain.fetchProjectsRecentlyPublished();
+    }
+}
