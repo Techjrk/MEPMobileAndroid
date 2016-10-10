060f61498d4dc835afd30499470221cd46bd6e63
Dushan Perera <dushan@domandtom.com>
2016-10-10 14:31:53 -0400

    Added LecetApplication and initializes Realm in onCreate.

---
+++ app/src/main/java/com/lecet/app/LecetApplication.java
@@ -0,0 +1,23 @@
+package com.lecet.app;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+/**
+ * File: LecetApplication Created: 10/10/16 Author: domandtom
+ *
+ * This code is copyright (c) 2016 Dom & Tom Inc.
+ */
+
+public class LecetApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+    }
+
+
+}
