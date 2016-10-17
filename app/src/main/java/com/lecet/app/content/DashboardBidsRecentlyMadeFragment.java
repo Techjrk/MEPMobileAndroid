d552d4329ea251da138412ed9063abd8a58e271a
Dushan Perera <dushan@domandtom.com>
2016-10-17 16:30:21 -0400

    DashboardBidsRecentlyMadeFragment implemented MBR Datasource and Delegate

--- app/src/main/java/com/lecet/app/content/DashboardBidsRecentlyMadeFragment.java
+++ app/src/main/java/com/lecet/app/content/DashboardBidsRecentlyMadeFragment.java
@@ -1,6 +1,8 @@
 package com.lecet.app.content;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,6 +11,13 @@ import android.view.ViewGroup;
 
 import com.lecet.app.R;
 import com.lecet.app.contentbase.DashboardChartFragmentBase;
+import com.lecet.app.data.models.Bid;
+import com.lecet.app.interfaces.LecetCallback;
+import com.lecet.app.interfaces.MBRDataSource;
+import com.lecet.app.interfaces.MBRDelegate;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  * Created by jasonm on 10/5/16.
@@ -17,6 +26,9 @@ import com.lecet.app.contentbase.DashboardChartFragmentBase;
 public class DashboardBidsRecentlyMadeFragment extends DashboardChartFragmentBase {
 
     private static final String TAG = "BidsRecentlyMadeFrag";
+
+    private MBRDelegate delegate;
+    private MBRDataSource dataSource;
 
 
     public static DashboardBidsRecentlyMadeFragment newInstance(int page, String title, String subtitle) {
@@ -35,4 +47,37 @@ public class DashboardBidsRecentlyMadeFragment extends DashboardChartFragmentBas
         super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            delegate = (MBRDelegate) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement MBRDelegate");
+        }
+
+        try {
+            dataSource = (MBRDataSource) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement MHSDataSource");
+        }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        dataSource.refreshRecentlyMadeBids(new LecetCallback<TreeMap<Long, TreeSet<Bid>>>() {
+            @Override
+            public void onSuccess(TreeMap<Long, TreeSet<Bid>> result) {
+
+            }
+
+            @Override
+            public void onFailure(int code, String message) {
+
+            }
+        });
+    }
 }
