c31d759da0f8d3131b5ae4180a47e0e90a704c73
Dushan Perera <dushan@domandtom.com>
2016-10-24 14:19:27 -0400

    Initial commit project detail activity.

---
+++ app/src/main/java/com/lecet/app/content/ProjectDetailActivity.java
@@ -0,0 +1,17 @@
+package com.lecet.app.content;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lecet.app.R;
+
+public class ProjectDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_detail);
+    }
+
+
+}
