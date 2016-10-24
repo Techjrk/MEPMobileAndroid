bc64f2023efa75becf2522315643eb7f356a6497
Dushan Perera <dushan@domandtom.com>
2016-10-24 16:39:56 -0400

    Added MRA & MRU data source and delegate.

---
+++ app/src/main/java/com/lecet/app/interfaces/MRUDataSource.java
@@ -0,0 +1,10 @@
+package com.lecet.app.interfaces;
+
+/**
+ * File: MRUDataSource Created: 10/24/16 Author: domandtom
+ *
+ * This code is copyright (c) 2016 Dom & Tom Inc.
+ */
+
+public interface MRUDataSource {
+}
