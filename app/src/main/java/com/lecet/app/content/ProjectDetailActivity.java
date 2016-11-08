083618d4903d6698ec71a7def714bb372fd49046
Dushan Perera <dushan@domandtom.com>
2016-11-08 12:14:21 -0500

    ProjectDetailActivity cleaned up class and added intent get extra call to get Project ID

--- app/src/main/java/com/lecet/app/content/ProjectDetailActivity.java
+++ app/src/main/java/com/lecet/app/content/ProjectDetailActivity.java
@@ -7,13 +7,20 @@ import android.support.v7.widget.RecyclerView;
 
 import com.lecet.app.R;
 import com.lecet.app.adapters.ProjectDetailAdapter;
+import com.lecet.app.data.api.LecetClient;
+import com.lecet.app.data.models.Project;
+import com.lecet.app.data.storage.LecetSharedPreferenceUtil;
+import com.lecet.app.domain.ProjectDomain;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import io.realm.Realm;
+
 public class ProjectDetailActivity extends AppCompatActivity {
 
-    // TODO: Test Adapter
+    public static final String PROJECT_ID_EXTRA = "com.lecet.app.content.ProjectDetailActivity.project.id.extra";
+
     private ProjectDetailAdapter detailAdapter;
 
     @Override
@@ -21,41 +28,13 @@ public class ProjectDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_detail);
 
-        // TODO: Testing Only
-        List<String> section1 = new ArrayList<>();
-        section1.add("0");
-        section1.add("1");
-        section1.add("2");
-        section1.add("3");
-        section1.add("4");
-        section1.add("5");
-        section1.add("6");
-        section1.add("7");
-        section1.add("8");
-        section1.add("9");
+        ProjectDomain projectDomain = new ProjectDomain(LecetClient.getInstance(), LecetSharedPreferenceUtil.getInstance(this), Realm.getDefaultInstance());
 
-        List<String> section2 = new ArrayList<>();
-        section2.add("10");
-        section2.add("11");
-        section2.add("12");
-        section2.add("13");
-        section2.add("14");
-        section2.add("15");
+        long projectId = getIntent().getLongExtra(PROJECT_ID_EXTRA, -1);
 
-        List<String> section3 = new ArrayList<>();
-        section3.add("16");
-        section3.add("17");
+        Project project = projectDomain.fetchProjectById(projectId);
 
-        List<String> section4 = new ArrayList<>();
-        section4.add("18");
-
-        List<List<String>> data = new ArrayList<>();
-        data.add(section1);
-        data.add(section2);
-        data.add(section3);
-        data.add(section4);
-
-        detailAdapter = new ProjectDetailAdapter(data);
+        detailAdapter = new ProjectDetailAdapter(project, this);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 
@@ -63,6 +42,5 @@ public class ProjectDetailActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(detailAdapter);
     }
-
 
 }
