7aebd788db2f55e8e5b903d53bc9a8cc5f9c49cd
Jason Mortara <jason.mortara@domandtom.com>
2016-12-05 15:28:07 -0500

    updated section title text color to blue in style. removed unused imports. removed commented setContentView calls and ‘end of method‘ comments. added button click to MainActivity to launch Search.

--- app/src/main/java/com/lecet/app/content/SearchActivity.java
+++ app/src/main/java/com/lecet/app/content/SearchActivity.java
@@ -1,12 +1,10 @@
 package com.lecet.app.content;
 
-import android.content.res.AssetManager;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
-import android.widget.TextView;
 
 import com.lecet.app.R;
 import com.lecet.app.data.api.LecetClient;
@@ -15,10 +13,6 @@ import com.lecet.app.databinding.ActivitySearchBinding;
 import com.lecet.app.domain.SearchDomain;
 import com.lecet.app.viewmodel.SearchViewModel;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import io.realm.Realm;
 
@@ -30,8 +24,6 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-       //setContentView(R.layout.list_item_search_recent_view);
-       // setContentView(R.layout.activity_search);
         setupBinding();
     }
 
@@ -40,7 +32,7 @@ public class SearchActivity extends AppCompatActivity {
         SearchViewModel viewModel = new SearchViewModel(this,
                 new SearchDomain(LecetClient.getInstance(), LecetSharedPreferenceUtil.getInstance(getApplication()), Realm.getDefaultInstance()));
         binding.setViewModel(viewModel);
-    } //end of setupBinding
+    }
 
 }
 
