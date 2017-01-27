c0f60da0a78be5e16d110e89c3531999128c9136
noel <noel.anonas@domandtom.com>
2017-01-27 03:29:40 -0500

    Removed all the unnecessary codes, logs and comments in SearchFilterMPFTypeViewModel class.

--- app/src/main/java/com/lecet/app/viewmodel/SearchFilterMPFTypeViewModel.java
+++ app/src/main/java/com/lecet/app/viewmodel/SearchFilterMPFTypeViewModel.java
@@ -7,11 +7,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.CheckBox;
 
 import com.lecet.app.data.models.SearchFilterProjectTypesMain;
-
-import java.util.ArrayList;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -25,10 +22,7 @@ public class SearchFilterMPFTypeViewModel extends BaseObservable {
     private final String TAG = "SearchFilterMPFTypeVM";
 
     private AppCompatActivity activity;
-
-    private ArrayList<String> listTypeDataResult = new ArrayList<String>();
     private Bundle pTypeData = new Bundle();
-//    private String[] ptype;     // = {"", ""};   // name, id
     private RealmResults<SearchFilterProjectTypesMain> realmProjectTypes;
 
     /**
@@ -37,7 +31,6 @@ public class SearchFilterMPFTypeViewModel extends BaseObservable {
     public SearchFilterMPFTypeViewModel(AppCompatActivity activity) {
         this.activity = activity;
         getProjectTypes();
-//        ptype = new String[2];
     }
 
     /**
@@ -60,33 +53,10 @@ public class SearchFilterMPFTypeViewModel extends BaseObservable {
      */
     public void onApplyButtonClicked(View view) {
         Intent intent = activity.getIntent();
-//        if (ptype == null || ptype.length == 1) ptype = new String[]{"",""};
- //       intent.putExtra(SearchViewModel.FILTER_EXTRA_DATA, ptype);
         intent.putExtra(SearchViewModel.FILTER_EXTRA_DATA_BUNDLE, pTypeData);
-//        activity.setResult(R.id.type & 0xfff, intent);
         activity.setResult(Activity.RESULT_OK, intent);
         activity.finish();
     }
-
-    /*public void onSelected(View view) {
-        ptype[0] = ((CheckBox) view).getText().toString();
-        ptype[1] = "103";   //(String)view.getTag();
-    }*/
-
-    /*public String[] getType() {
-        return ptype;
-    }*/
-
-   /*public void setTypeName(String name, String id) {
-        Log.d("ptype","ptype size"+ptype.length);
-        if (ptype == null || ptype.length == 1) ptype = new String[2];
-        ptype[0] = name;
-        ptype[1] = id;
-    }*/
-
-    /*public void setType(String[] ptype) {
-        this.ptype = ptype;
-    }*/
 
     public RealmResults<SearchFilterProjectTypesMain> getRealmProjectTypes() {
         return realmProjectTypes;
@@ -103,22 +73,4 @@ public class SearchFilterMPFTypeViewModel extends BaseObservable {
         pTypeData.remove(key);
         Log.d(TAG, "addPTypeData: pTypeData is now: " + pTypeData);
     }
-
-    public ArrayList<String> getListTypeDataResult() {
-        return listTypeDataResult;
-    }
-
-    public void setListTypeDataResult(ArrayList<String> listTypeDataResult) {
-        this.listTypeDataResult = listTypeDataResult;
-    }
-
-    public void addListTypeDataResult(String data) {
-        this.listTypeDataResult.add(data);
-    }
-    public void removeListTypeDataResult(String data) {
-        this.listTypeDataResult.remove(data);
-    }
-
-
-
 }
