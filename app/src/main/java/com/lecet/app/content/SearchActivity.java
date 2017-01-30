a0968b567bc0f9485af7f65edd68e3862fae111e
Noel Anonas <noel.anonas@domandtom.com>
2017-01-30 10:44:47 -0500

    Commented the toast message in SearchActivity class

--- app/src/main/java/com/lecet/app/content/SearchActivity.java
+++ app/src/main/java/com/lecet/app/content/SearchActivity.java
@@ -50,7 +50,7 @@ public class SearchActivity extends AppCompatActivity { //test comment - ignore
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Toast.makeText(this, "Search Activity Result request 1: " + requestCode + "  result: " + resultCode, Toast.LENGTH_SHORT).show();    //TODO - Toast
+  //      Toast.makeText(this, "Search Activity Result request 1: " + requestCode + "  result: " + resultCode, Toast.LENGTH_SHORT).show();    //TODO - Toast
 
         String locationFilter = "";
         String primaryProjectTypeFilter = "";
