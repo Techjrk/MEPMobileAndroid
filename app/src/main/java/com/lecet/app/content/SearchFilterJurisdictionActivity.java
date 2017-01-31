0a4b03aaa870f484eddb169b02ed1487ff000b07
Noel Anonas <noel.anonas@domandtom.com>
2017-01-31 08:30:30 -0500

    - Created and implemented the new method in SearchFilterJurisdictionActivity class called searchItem() to process the data entry made by the user in the jurisdiction search field. This is where the logic code to process filtering the jurisdiction items and to display the filtered view.

--- app/src/main/java/com/lecet/app/content/SearchFilterJurisdictionActivity.java
+++ app/src/main/java/com/lecet/app/content/SearchFilterJurisdictionActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.WindowManager;
 
 import com.lecet.app.R;
@@ -30,13 +31,15 @@ public class SearchFilterJurisdictionActivity extends AppCompatActivity {
         ActivitySearchFilterJurisdictionBinding sfilter = DataBindingUtil.setContentView(this, R.layout.activity_search_filter_jurisdiction);
         SearchFilterJurisdictionViewModel viewModel = new SearchFilterJurisdictionViewModel(this);
         sfilter.setViewModel(viewModel);
-        initRecycleView(viewModel);
+       // initRecycleViewMain(viewModel);
+        searchItem(viewModel,"");
     }
 
-    /**
+    /**g
      * Process the multi-level display item of the jurisdiction with adapter
      */
-    public void initRecycleView(SearchFilterJurisdictionViewModel viewModel) {
+/*
+    public void initRecycleViewMain(SearchFilterJurisdictionViewModel viewModel) {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.test_recycler_view);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(mLayoutManager);
@@ -103,5 +106,104 @@ public class SearchFilterJurisdictionActivity extends AppCompatActivity {
         SearchFilterJurisdictionAdapter adapter = new SearchFilterJurisdictionAdapter(data, viewModel);
         recyclerView.setAdapter(adapter);
     }
+*/
 
+    /**
+     * Process the search Item  of thejurisdiction typed by the user
+     */
+    boolean foundKey1, foundKey2, foundKey3,foundKey4;
+    ArrayList<String> foundMain= new ArrayList<String>();
+    public void searchItem(SearchFilterJurisdictionViewModel viewModel, String key) {
+        String searchKey=key;
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.test_recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        SearchFilterJurisdictionAdapter.GrandChild grandChild = new SearchFilterJurisdictionAdapter.GrandChild();
+
+        List<SearchFilterJurisdictionAdapter.Parent> data = new ArrayList<>();
+
+//        int ctr = 0;
+
+        List<SearchFilterJurisdictionAdapter.Child> children = null;
+
+        for (SearchFilterJurisdictionMain jMain : viewModel.getRealmJurisdictions()) {
+            foundKey1=false; foundMain.clear();
+            SearchFilterJurisdictionAdapter.Parent parent = new SearchFilterJurisdictionAdapter.Parent();
+            parent.setId(jMain.getId());
+            parent.setName(jMain.getName());
+            parent.setAbbreviation(jMain.getAbbreviation());
+            parent.setLongName(jMain.getLongName());
+            if (parent.getName().contains(searchKey)) {
+                foundKey1=true; foundMain.add("jmain");
+            }
+            //  ctr++;
+
+            // Children (District Councils)
+            children = new ArrayList<>();
+            // int childctr=0;
+            for (SearchFilterJurisdictionLocal jlocal : jMain.getLocals()) {
+                if (jlocal != null) {
+                    foundKey2=false;
+                    SearchFilterJurisdictionAdapter.Child child = new SearchFilterJurisdictionAdapter.Child();
+                    child.setId(jlocal.getId());
+                    child.setName(jlocal.getName());
+                    child.setDistrictCouncilId(jlocal.getDistrictCouncilId());
+                    if (child.getName().trim().contains(searchKey)) {foundKey2=true; foundMain.add("jlocal");}
+
+                    if (foundKey2 || foundMain.contains("jmain")) children.add(child);
+
+
+                }
+            }
+            // parent.setSubtypes(subtypes);
+            //   int childctr2=0;
+            for (SearchFilterJurisdictionDistrictCouncil dcouncil : jMain.getDistrictCouncils()) {
+                if (dcouncil != null) {
+//                    ctr++;
+                    foundKey3=false;
+                    SearchFilterJurisdictionAdapter.Child child = new SearchFilterJurisdictionAdapter.Child();
+                    child.setName(dcouncil.getName());
+                    if (child.getName().trim().contains(searchKey)) {
+                        foundKey3=true; foundMain.add("dcouncil");
+                        Log.d("found3","found3"+child.getName());
+                    }
+                    if (dcouncil.getLocals() != null) {
+
+                        List<SearchFilterJurisdictionAdapter.GrandChild> grandChildren1 = new ArrayList<>();
+//                        List<SearchFilterJurisdictionAdapter.SubSubtype> subSubtypes = new ArrayList<>();
+
+                        // Locals
+                        for (SearchFilterJurisdictionLocal dclocals : dcouncil.getLocals()) {
+                            if (dclocals != null) {
+                                foundKey4=false;
+                                SearchFilterJurisdictionAdapter.GrandChild grandChild1 = new SearchFilterJurisdictionAdapter.GrandChild();
+                                grandChild1.setId(dclocals.getId());
+                                grandChild1.setName(dclocals.getName());
+                                if (grandChild1.getName().contains(searchKey)) {foundKey4=true;foundMain.add("dclocal");}
+                                if (foundKey4 || foundKey3 || foundMain.contains("jmain")  ) grandChildren1.add(grandChild1);
+                            }
+//                        if (dclocals !=null)   Log.d("jdcouncillocals","jdcouncillocals = name:"+dclocals.getName()+" id:"+dclocals.getId()+" dcid:"+dclocals.getDistrictCouncilId());
+                        }
+                        if (child !=null && grandChildren1 !=null)  child.setGrandChildren(grandChildren1);
+                    }
+                    if (foundKey3 ||  foundMain.contains("jmain") || foundMain.contains("dclocal")  ) {
+                        children.add(child);
+                        foundMain.remove("dclocal");
+                       // foundMain.remove("dcouncil");
+                        Log.d("child added","child added");
+                    }
+                }
+            }
+            if (children !=null) {
+                parent.setChildren(children);
+             //   foundMain.remove("dcouncil");
+                Log.d("parent added","parent added");
+            }
+            if (parent !=null && foundMain.size() > 0) data.add(parent);
+        }
+
+        SearchFilterJurisdictionAdapter adapter = new SearchFilterJurisdictionAdapter(data, viewModel);
+        recyclerView.setAdapter(adapter);
+    }
 }
