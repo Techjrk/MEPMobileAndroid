dadadf2438104e6e3fbef5bdc941d74387acf1ed
Dushan Perera <dushan@domandtom.com>
2017-02-01 16:51:50 -0500

    LocationDetailActivity edits.

--- app/src/main/java/com/lecet/app/content/LocationDetailMapActivity.java
+++ app/src/main/java/com/lecet/app/content/LocationDetailMapActivity.java
@@ -10,6 +10,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import com.lecet.app.R;
+
 public class LocationDetailMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
