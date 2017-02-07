f2c9bc2c4e6af9283ff114808eba6fe7635fed31
Noel Anonas <noel.anonas@domandtom.com>
2017-02-07 05:34:32 -0500

    Transferred the logic code of processing the search view in the Project Type activity class to the SearchFilterProjectTypeViewModel class.

--- app/src/main/java/com/lecet/app/content/SearchFilterStageActivity.java
+++ app/src/main/java/com/lecet/app/content/SearchFilterStageActivity.java
@@ -28,44 +28,6 @@ public class SearchFilterStageActivity extends AppCompatActivity {
         ActivitySearchFilterStageBinding sfilter = DataBindingUtil.setContentView(this, R.layout.activity_search_filter_stage);
         SearchFilterStageViewModel viewModel = new SearchFilterStageViewModel(this);
         sfilter.setViewModel(viewModel);
-        initRecycleView(viewModel);
+     //   initRecycleView(viewModel);
     }
-
-    /**
-     * Process the multi-level display item of the Stage with adapter
-     */
-    public void initRecycleView(SearchFilterStageViewModel viewModel) {
-        Log.d("SearchFilterMPFStageAct", "stageinit");
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(mLayoutManager);
-
-        List<SearchFilterStageAdapter.Parent> data = new ArrayList<>();
-
-        List<SearchFilterStageAdapter.Child> children = null;
-        for (SearchFilterStagesMain parentStage : viewModel.getRealmStages()) {
-
-            Log.d("SearchFilterMPFStageAct", "stagelist: name: " + parentStage.getName());
-            SearchFilterStageAdapter.Parent parent = new SearchFilterStageAdapter.Parent();
-            parent.setId(parentStage.getId());
-            parent.setName(parentStage.getName());
-
-            children = new ArrayList<>();
-            for (SearchFilterStage childStage : parentStage.getStages()) {
-                if (childStage != null) {
-                    SearchFilterStageAdapter.Child child = new SearchFilterStageAdapter.Child();
-                    child.setId(childStage.getId());
-                    child.setName(childStage.getName());
-                    children.add(child);
-                }
-            }
-            parent.setChildren(children);
-            data.add(parent);
-        }
-
-        Log.d("SearchFilterMPFStageAct", "stagemain size: "+viewModel.getRealmStages().size());
-        SearchFilterStageAdapter adapter = new SearchFilterStageAdapter(data, viewModel);
-        recyclerView.setAdapter(adapter);
-    }
-
 }
