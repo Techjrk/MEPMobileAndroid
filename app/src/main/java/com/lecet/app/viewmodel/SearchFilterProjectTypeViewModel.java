4371102f08693f2b390230945354bc262930da0b
Noel Anonas <noel.anonas@domandtom.com>
2017-02-07 04:57:15 -0500

    Transferred the logic code of processing of search in the Project Type activity class  to the SearchFilterProjectTypeViewModel. In SearchFilterProjectTypeViewModel class: Added a data-binding query variable with getter and setter method for the ProjectType search field. In SearchFilterProjectTypeViewModel class: Added the searchItem()  to process the search item typed by the user in the ProjectType search field section.

--- app/src/main/java/com/lecet/app/viewmodel/SearchFilterProjectTypeViewModel.java
+++ app/src/main/java/com/lecet/app/viewmodel/SearchFilterProjectTypeViewModel.java
@@ -3,12 +3,23 @@ package com.lecet.app.viewmodel;
 import android.app.Activity;
 import android.content.Intent;
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
+import com.lecet.app.BR;
+import com.lecet.app.R;
+import com.lecet.app.adapters.SearchFilterProjectTypeAdapter;
+import com.lecet.app.data.models.PrimaryProjectType;
 import com.lecet.app.data.models.SearchFilterProjectTypesMain;
+import com.lecet.app.data.models.SearchFilterProjectTypesProjectCategory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -18,11 +29,25 @@ import io.realm.RealmResults;
  * Created by getdevsinc on 12/29/16.
  */
 public class SearchFilterProjectTypeViewModel extends BaseObservable {
-    private final String TAG = "SearchFilterProjTypeVM";
+    private boolean foundParent, foundChild, foundGrandChild, hasChild, hasGrandChild;
+    private ArrayList<Boolean> containGrandChild = new ArrayList<Boolean>();
 
+    private final String TAG = "SearchFilterProjTypeVM";
     private AppCompatActivity activity;
     private Bundle bundle;
     private RealmResults<SearchFilterProjectTypesMain> realmProjectTypes;
+    private String query;
+
+    @Bindable
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+        notifyPropertyChanged(BR.query);
+        searchItem(query);
+    }
 
     /**
      * Constructor
@@ -31,6 +56,7 @@ public class SearchFilterProjectTypeViewModel extends BaseObservable {
         this.activity = activity;
         bundle = new Bundle();
         getProjectTypes();
+        searchItem("");
     }
 
     /**
@@ -68,5 +94,87 @@ public class SearchFilterProjectTypeViewModel extends BaseObservable {
 
     public void removeProjectTypeData(String key) {
         bundle.remove(key);
+    }
+
+    public void searchItem(String key) {
+        foundParent = false;
+        foundChild = false;
+        foundGrandChild = false;
+        hasChild = false;
+        hasGrandChild = false;
+        String searchKey = key;
+
+        Log.d("typeinit", "typeinit");
+        RecyclerView recyclerView = (RecyclerView) activity.findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(activity);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        List<SearchFilterProjectTypeAdapter.Parent> data = new ArrayList<>();
+        List<SearchFilterProjectTypeAdapter.Child> children = null;
+        for (SearchFilterProjectTypesMain ptMain : getRealmProjectTypes()) {
+            hasGrandChild = false;
+            hasChild = false;
+            foundParent = false;
+            foundChild = false;
+            foundGrandChild = false;
+            containGrandChild.clear();
+            SearchFilterProjectTypeAdapter.Parent parent = new SearchFilterProjectTypeAdapter.Parent();
+            parent.setName(ptMain.getTitle());
+            parent.setId("" + ptMain.getId());
+            if (parent.getName().trim().toLowerCase().contains(searchKey.trim().toLowerCase())) {
+                foundParent = true;
+            }
+            children = new ArrayList<>();
+            for (SearchFilterProjectTypesProjectCategory ptpc : ptMain.getProjectCategories()) {
+                if (ptpc != null) {
+                    foundChild = false;
+                    containGrandChild.clear();
+                    SearchFilterProjectTypeAdapter.Child child = new SearchFilterProjectTypeAdapter.Child();
+                    child.setName(ptpc.getTitle());
+                    child.setId("" + ptpc.getId());
+                    if (child.getName().trim().toLowerCase().contains(searchKey.toLowerCase())) {
+                        hasChild = true;
+                        foundChild = true;
+                        Log.d("foundChild", "foundChild" + child.getName());
+                    }
+                    List<PrimaryProjectType> gchildTypes = ptpc.getProjectTypes();
+                    List<SearchFilterProjectTypeAdapter.GrandChild> grandChildren = new ArrayList<>();
+                    for (PrimaryProjectType ppType : gchildTypes) {
+                        if (ppType != null) {
+                            foundGrandChild = false;
+                            SearchFilterProjectTypeAdapter.GrandChild gchild = new SearchFilterProjectTypeAdapter.GrandChild();
+                            gchild.setName(ppType.getTitle());
+                            gchild.setId("" + ppType.getId());
+                            if (gchild.getName().toLowerCase().contains(searchKey.toLowerCase())) {
+                                hasGrandChild = true;
+                                foundGrandChild = true;
+                                containGrandChild.add(hasGrandChild);
+                            }
+                            // grandChildren.add(gchild);
+                            if (foundGrandChild || foundChild || foundParent)
+                                grandChildren.add(gchild);
+                        }
+                    }
+                    if (child != null && grandChildren != null)
+                        child.setGrandChildren(grandChildren);
+                    //   children.add(child);
+                    if ((foundChild || containGrandChild.contains(true)) || foundParent) {
+                        children.add(child);
+                        containGrandChild.remove(true);
+                        Log.d("child3 added", "haschild3: " + hasChild + "hasgchild" + hasGrandChild + "child1 added" + child.getName() + " f1:" + foundParent + " f3:" + foundChild + " fdc:" + containGrandChild.contains(true));
+                    }
+                }
+
+            }
+            // parent.setChildren(children);
+            // data.add(parent);
+            if (children != null) {
+                parent.setChildren(children);
+                Log.d("parent1 added", "parent1 added");
+            }
+            if (parent != null && (hasChild || hasGrandChild) || foundParent) data.add(parent);
+        }
+        SearchFilterProjectTypeAdapter adapter = new SearchFilterProjectTypeAdapter(data, this);
+        recyclerView.setAdapter(adapter);
     }
 }
