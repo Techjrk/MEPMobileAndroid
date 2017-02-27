f85d8b3caa39144fb54599c07d3e6619e3211f29
Noel Anonas <noel.anonas@domandtom.com>
2017-02-27 06:25:33 -0500

    Replaced the array of strings to bundle type object. Modified and added codes in the constructor, onApplyButton(), onSelected() and onCancelButtonClick() to be used for processing the bundle object. Created a setupUpdatedWithinData() and setBundleData() to store data as the key and value pair to the bundle object. Created some instance variables with getter and setter methods to be used for bundle object. Removed all the codes related to the array of Strings.

--- app/src/main/java/com/lecet/app/viewmodel/SearchFilterUpdatedWithinViewModel.java
+++ app/src/main/java/com/lecet/app/viewmodel/SearchFilterUpdatedWithinViewModel.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RadioButton;
+
+import com.lecet.app.BR;
 
 /**
  * Created by getdevsinc on 12/29/16.
@@ -14,42 +17,83 @@ import android.widget.RadioButton;
 
 public class SearchFilterUpdatedWithinViewModel extends BaseObservable {
     private AppCompatActivity activity;
-
-    private String[] time = {"Any", "0"};
+    public static final String BUNDLE_KEY_DISPLAY_STR = "com.lecet.app.viewmodel.SearchFilterUpdatedWithinViewModel.display.displayText.extra";
+    public static final String BUNDLE_KEY_DAYS_INT    = "com.lecet.app.viewmodel.SearchFilterUpdatedWithinViewModel.display.daysInt.extra";
+    private String displayStr = "Any";
+    private String daysInt = "0";
+   // private String[] time = {"Any", "0"};
+    private Bundle bundle;
 
     /**
      * Constructor
      */
     public SearchFilterUpdatedWithinViewModel(AppCompatActivity activity) {
         this.activity = activity;
+        bundle = new Bundle();
     }
 
     public void onApplyButtonClick(View view) {
         Intent intent = activity.getIntent();
-        intent.putExtra(SearchViewModel.FILTER_EXTRA_DATA, time);
+//        intent.putExtra(SearchViewModel.FILTER_EXTRA_DATA, time);
+        intent.putExtra(SearchViewModel.FILTER_EXTRA_DATA_BUNDLE, bundle);
         activity.setResult(Activity.RESULT_OK, intent);
         activity.finish();
     }
 
     public void onCancelButtonClick(View view) {
         activity.setResult(Activity.RESULT_CANCELED);
-        time[0]="Any"; time[1]="0";   //reset the selection
+        //time[0]="Any"; time[1]="0";   //reset the selection
         activity.finish();
     }
 
     public void onSelected(View view) {
-        time[0] = ((RadioButton) view).getText().toString();
-        time[1] = (String) view.getTag();
+        String displayStr = ((RadioButton) view).getText().toString();
+        String daysInt = (String) view.getTag();
+        setUpdatedWithinData(displayStr,daysInt);
+        //time[0] = ((RadioButton) view).getText().toString();
+        //time[1] = (String) view.getTag();
+    }
+
+    public void setUpdatedWithinData(String displayStr, String daysInt) {
+        setDisplayStr(displayStr);
+        setDaysInt(daysInt);
+
+        // overwrite the Bundle instance with each selection since Bidding Within only supports single-selection
+        bundle = new Bundle();
+        setBundleData(BUNDLE_KEY_DISPLAY_STR, this.displayStr);
+        setBundleData(BUNDLE_KEY_DAYS_INT, this.daysInt);
+    }
+
+    private void setBundleData(String key, String value) {
+        bundle.putString(key, value);
     }
 
     @Bindable
+    public String getDisplayStr() {
+        return displayStr;
+    }
+
+    public void setDisplayStr(String displayStr) {
+        this.displayStr = displayStr;
+    }
+
+    @Bindable
+    public String getDaysInt() {
+        return daysInt;
+    }
+
+    public void setDaysInt(String daysInt) {
+        this.daysInt = daysInt;
+        notifyPropertyChanged(BR.daysInt);
+    }
+  /*  @Bindable
     public String[] getTime() {
         return time;
     }
 
     public void setTime(String[] time) {
         this.time = time;
-    }
+    }*/
 
 
 }
