ad170e37520058bc0926f4fee2f88abd77334b6c
Noel Anonas <noel.anonas@domandtom.com>
2017-02-27 06:13:36 -0500

    Modified and added codes in the onCreate() to correctly process the updated within section with the use of the bundle object.

--- app/src/main/java/com/lecet/app/content/SearchFilterUpdatedWithinActivity.java
+++ app/src/main/java/com/lecet/app/content/SearchFilterUpdatedWithinActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.lecet.app.R;
 import com.lecet.app.databinding.ActivitySearchFilterUpdatedWithinBinding;
+import com.lecet.app.viewmodel.SearchFilterBiddingWithinViewModel;
 import com.lecet.app.viewmodel.SearchFilterUpdatedWithinViewModel;
 import com.lecet.app.viewmodel.SearchFilterMPFViewModel;
 
@@ -16,15 +17,26 @@ public class SearchFilterUpdatedWithinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivitySearchFilterUpdatedWithinBinding sfilter = DataBindingUtil.setContentView(this, R.layout.activity_search_filter_updated_within);
+        SearchFilterUpdatedWithinViewModel viewModel = new SearchFilterUpdatedWithinViewModel(this);
 
+        // get passed data from Intent if returning to Activity with existing user selection
+        Intent intent = getIntent();
+        String displayStr   = intent.getStringExtra(SearchFilterMPFViewModel.EXTRA_UPDATED_WITHIN_DISPLAY_STR);
+        String daysInt      = intent.getStringExtra(SearchFilterMPFViewModel.EXTRA_UPDATED_WITHIN_DAYS_INT);
+        if(displayStr != null && daysInt != null) {
+            viewModel.setUpdatedWithinData(displayStr, daysInt);
+        }
+
+        sfilter.setViewModel(viewModel);
+       /*
         // get Updated Within Extras
         Intent intent = getIntent();
         String[] updatedWithinArr = intent.getStringArrayExtra(SearchFilterMPFViewModel.EXTRA_UPDATED_WITHIN);
 
         SearchFilterUpdatedWithinViewModel viewModel = new SearchFilterUpdatedWithinViewModel(this);
         if(updatedWithinArr != null) {
-            viewModel.setTime(updatedWithinArr);
+           // viewModel.setTime(updatedWithinArr);
         }
-        sfilter.setViewModel(viewModel);
+        sfilter.setViewModel(viewModel);*/
     }
 }
