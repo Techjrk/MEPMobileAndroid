30a930568060dfcf755b7308fed9e5f6bb520246
Noel Anonas <noel.anonas@domandtom.com>
2017-02-28 07:48:29 -0500

    Converted the multiple item selection into correct single item selection for Owner Type section. Created an instance variable to get the current text display. Created a class variable to monitor the state of the checkbox. Modified and added codes in the constructor to add the process of checking the state of the checkbox. Modified and added codes in the onSelected() to add the process of making a single-select item instead of multiple item selection.

--- app/src/main/java/com/lecet/app/viewmodel/SearchFilterOwnerTypeViewModel.java
+++ app/src/main/java/com/lecet/app/viewmodel/SearchFilterOwnerTypeViewModel.java
@@ -3,7 +3,9 @@ package com.lecet.app.viewmodel;
 import android.app.Activity;
 import android.content.Intent;
 import android.databinding.BaseObservable;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 
@@ -13,29 +15,56 @@ import android.widget.CheckBox;
 
 public class SearchFilterOwnerTypeViewModel extends BaseObservable {
     private AppCompatActivity activity;
+    private String displayStr = "Any";
+    public static CheckBox lastChecked;
 
     private String[] ownertype = {"", ""};
-
+    //private CheckBox checkView;
     /**
      * Constructor
      */
     public SearchFilterOwnerTypeViewModel(AppCompatActivity activity) {
+
         this.activity = activity;
+        if (lastChecked != null) {
+            CheckBox cb = (CheckBox)  activity.findViewById(lastChecked.getId())  ;
+            cb.setChecked(true);
+           lastChecked = cb;
+        //    lastChecked.notify();
+       //     Log.d("lastchecked","lastchecked"+lastChecked);
+        }
+
     }
 
     public void onApplyButtonClick(View view) {
         Intent intent = activity.getIntent();
-        intent.putExtra(SearchViewModel.FILTER_EXTRA_DATA, ownertype);
+        //intent.putExtra(SearchViewModel.FILTER_EXTRA_DATA_BUNDLE, bundle);
+         intent.putExtra(SearchViewModel.FILTER_EXTRA_DATA, ownertype);
         activity.setResult(Activity.RESULT_OK, intent);
         activity.finish();
     }
 
     public void onCancelButtonClick(View view) {
         activity.setResult(Activity.RESULT_CANCELED);
+     //   lastChecked=null;
         activity.finish();
     }
-
+    
     public void onSelected(View view) {
-        ownertype[0] = ((CheckBox) view).getText().toString();
+
+        CheckBox cb = (CheckBox) view;
+        //        ownertype[0] = ((CheckBox) view).getText().toString();
+        displayStr = cb.getText().toString();
+        ownertype[0] = displayStr;
+        if (cb.isChecked()) {
+            if (lastChecked != null) {
+                lastChecked.setChecked(false);
+               // checkLastSelect(false);
+            }
+            lastChecked = cb;
+        }
+
+
+
     }
 }
