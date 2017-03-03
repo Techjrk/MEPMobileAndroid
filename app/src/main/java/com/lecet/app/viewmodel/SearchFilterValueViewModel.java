3cc41b7412b9490bc0fa72a4f220ba86ea77c89e
Jason Mortara <jason.mortara@domandtom.com>
2017-03-03 17:32:40 -0500

    fix for 6720 - MSE: Filter - Values field issues

--- app/src/main/java/com/lecet/app/viewmodel/SearchFilterValueViewModel.java
+++ app/src/main/java/com/lecet/app/viewmodel/SearchFilterValueViewModel.java
@@ -1,11 +1,15 @@
 package com.lecet.app.viewmodel;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import com.lecet.app.R;
 
 /**
  * Created by getdevsinc on 12/29/16.
@@ -14,6 +18,7 @@ import android.view.View;
 public class SearchFilterValueViewModel extends BaseObservable {
     private AppCompatActivity activity;
     private String min = "", max = "";
+    private AlertDialog minMaxDialog = null;
 
     /**
      * Constructor
@@ -23,10 +28,41 @@ public class SearchFilterValueViewModel extends BaseObservable {
     }
 
     public void onApplyButtonClick(View view) {
-        Intent intent = activity.getIntent();
-        intent.putExtra(SearchViewModel.FILTER_EXTRA_DATA, new String[]{min, max});
-        activity.setResult(Activity.RESULT_OK, intent);
-        activity.finish();
+
+        // min/max validation
+        Integer minInt = Integer.valueOf(getMin());
+        Integer maxInt = Integer.valueOf(getMax());
+        if (minInt <= maxInt) {
+            Intent intent = activity.getIntent();
+            intent.putExtra(SearchViewModel.FILTER_EXTRA_DATA, new String[]{min, max});
+            activity.setResult(Activity.RESULT_OK, intent);
+            activity.finish();
+        }
+        else {
+            showMinMaxDialog();
+        }
+    }
+
+    /**
+     * Show the min/max validation dialog
+     */
+    public void showMinMaxDialog() {
+        if (minMaxDialog != null && minMaxDialog.isShowing()) {
+            minMaxDialog.dismiss();
+        }
+
+        minMaxDialog = new AlertDialog.Builder(this.activity).create();
+        minMaxDialog.setTitle(this.activity.getResources().getString(R.string.value_min_max_dialog_title));
+        minMaxDialog.setMessage(this.activity.getResources().getString(R.string.value_min_max_dialog_msg));
+
+        minMaxDialog.setButton(AlertDialog.BUTTON_POSITIVE, this.activity.getResources().getString(android.R.string.ok),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        minMaxDialog.show();
     }
 
     public void onCancelButtonClick(View view) {
