b9e4136d1e4c4890ef5f50116e2fd53f1ba91956
Ali Onque <ali@domandtom.com>
2017-03-30 14:06:16 -0400

    **Changes** Added functionality to the character size **Known Errors** Cancel and Add Button Do not work **Next Process** Get Cancel and Add Button to work

---
+++ app/src/main/java/com/lecet/app/data/models/BindableString.java
@@ -0,0 +1,40 @@
+package com.lecet.app.data.models;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.renderscript.Sampler;
+
+public class BindableString extends BaseObservable {
+    private String value;
+    private int size;
+
+    //Constructer sets up value size relation
+    public BindableString(String value){
+        super();
+        this.value = value;
+        size = getValue().length();
+        notifyChange();
+    }
+
+    @Bindable//@Bindable makes an Observable object visible to dataBinded xml
+    public String getValue() {
+      if(value.equals("") || value == null){//logic to avoid null strings giving errors
+          return "";
+      }else {
+          return value;
+      }
+    }
+
+    //Method called when the
+    public void setValue(String value) {
+      if (!this.value.equals(value)) {
+          this.value = value;
+          size = getValue().length();
+          notifyChange();
+      }
+
+    }
+
+    @Bindable
+    public String getSize(){return String.valueOf(size);}
+}
--- app/src/main/java/com/lecet/app/viewmodel/ProjectDetailAddNoteViewModel.java
+++ app/src/main/java/com/lecet/app/viewmodel/ProjectDetailAddNoteViewModel.java
@@ -2,8 +2,12 @@ package com.lecet.app.viewmodel;
 
 import android.content.Intent;
 import android.databinding.Bindable;
+import android.databinding.ObservableField;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+import com.lecet.app.data.models.BindableString;
 
 /**
  * Created by ludwigvondrake on 3/24/17.
@@ -11,10 +15,29 @@ import android.view.View;
 
 public class ProjectDetailAddNoteViewModel extends AppCompatActivity{
 
+    private static final String TAG = "projDetailAddNoteVM";
+    private BindableString noteTitle;
+
+    private String body = "";
+
     public ProjectDetailAddNoteViewModel() {
+        noteTitle = new BindableString("");
+    }
+
+    public BindableString getNoteTitle() {
+        return noteTitle;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
     }
 
     public void onClickCancel(View view){
         finish();
+        Log.e(TAG, "onClickCancel: onClickCancel called");
     }
 }
