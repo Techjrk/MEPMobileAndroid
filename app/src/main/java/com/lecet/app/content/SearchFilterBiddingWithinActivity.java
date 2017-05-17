0f38e1827d152e97de4c05f7f7d43146d40f4ca8
Noel Anonas <noel.anonas@domandtom.com>
2017-05-17 02:17:18 -0400

    Adding the return to home action bar to the Bidding Within search filter section. In SearchFilterBiddingWithinActivity class, added the setupToolbar() to include the implementation for “return to home” android native button. Included the action bar layout in the activity_search_filter_bidding_within.xml.

--- app/src/main/java/com/lecet/app/content/SearchFilterBiddingWithinActivity.java
+++ app/src/main/java/com/lecet/app/content/SearchFilterBiddingWithinActivity.java
@@ -3,7 +3,9 @@ package com.lecet.app.content;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.WindowManager;
 
 import com.lecet.app.R;
@@ -33,5 +35,17 @@ public class SearchFilterBiddingWithinActivity extends AppCompatActivity {
         }
 
         sfilter.setViewModel(viewModel);
+        setupToolbar();
+    }
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayShowCustomEnabled(false);
+        }
     }
 }
