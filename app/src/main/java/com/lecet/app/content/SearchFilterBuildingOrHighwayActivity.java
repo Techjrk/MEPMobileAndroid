b3de05a50caab0f63ba8af2fe42269b9c0d07622
Noel Anonas <noel.anonas@domandtom.com>
2017-05-17 02:19:39 -0400

    Adding the return to home action bar to the Building or Highway search filter section. In SearchFilterBuildingOrHighwayActivity class, added the setupToolbar() to include the implementation for “return to home” android native button. Included the action bar layout in the activity_search_filter_building_or_highway.xml

--- app/src/main/java/com/lecet/app/content/SearchFilterBuildingOrHighwayActivity.java
+++ app/src/main/java/com/lecet/app/content/SearchFilterBuildingOrHighwayActivity.java
@@ -2,13 +2,15 @@ package com.lecet.app.content;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.lecet.app.R;
 import com.lecet.app.databinding.ActivitySearchFilterBuildingOrHighwayBinding;
-import com.lecet.app.viewmodel.SearchFilterBuildingOrHighwayViewModel;
 import com.lecet.app.viewmodel.SearchFilterAllTabbedViewModel;
+import com.lecet.app.viewmodel.SearchFilterBuildingOrHighwayViewModel;
 
 public class SearchFilterBuildingOrHighwayActivity extends AppCompatActivity {
 
@@ -22,12 +24,24 @@ public class SearchFilterBuildingOrHighwayActivity extends AppCompatActivity {
         String[] bhArr = intent.getStringArrayExtra(SearchFilterAllTabbedViewModel.EXTRA_BUILDING_OR_HIGHWAY);
 
         SearchFilterBuildingOrHighwayViewModel viewModel = new SearchFilterBuildingOrHighwayViewModel(this);
-        if(bhArr != null) {
-         //   viewModel.setBh(bhArr);
+        if (bhArr != null) {
+            //   viewModel.setBh(bhArr);
             viewModel.setStrBH(bhArr[0]);
             viewModel.setTagBH(bhArr[1]);
         }
         sfilter.setViewModel(viewModel);
+        setupToolbar();
+    }
 
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayShowCustomEnabled(false);
+        }
     }
 }
