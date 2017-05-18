0390a3cc6b4b0804abf96c0275d5c15fbfbc222e
Noel Anonas <noel.anonas@domandtom.com>
2017-05-18 06:21:52 -0400

    Deleted the unnecessary LinearLayout type variables and getting this reference from the inner class RecycleView.ViewHolder class. Deleted the unnecessary long pressed event method and its implementation (long pressed from wireframe design is no longer included).

--- app/src/main/java/com/lecet/app/adapters/TrackingListAdapter.java
+++ app/src/main/java/com/lecet/app/adapters/TrackingListAdapter.java
@@ -1,29 +1,20 @@
 package com.lecet.app.adapters;
 
-import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-
 import com.lecet.app.R;
-
-import com.lecet.app.content.MainActivity;
-import com.lecet.app.content.ReturnToHomeActivity;
 import com.lecet.app.databinding.ListItemTrackingBinding;
 import com.lecet.app.viewmodel.TrackingListItem;
-
 import io.realm.RealmResults;
 
 import static com.lecet.app.R.string.google_api_key;
 
 /**
  * File: TrackingListRecyclerViewAdapter Created: 10/21/16 Author: domandtom
- *
+ * <p>
  * This code is copyright (c) 2016 Dom & Tom Inc.
  */
 
@@ -63,18 +54,7 @@ public abstract class TrackingListAdapter<T extends RealmResults> extends Recycl
     public void onBindViewHolder(final TrackingListViewHolderNew holder, int position) {
 
         final String mapsApiKey = appCompatActivity.getBaseContext().getResources().getString(google_api_key);
-
         holder.getBinding().setViewModel(viewModelForPosition(mapsApiKey, position, showUpdates));
-        holder.linearTrackHolder.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Log.d("longpressadapter","longpressadapter");
-                Intent intent = new Intent(getAppCompatActivity(), ReturnToHomeActivity.class);
-                getAppCompatActivity().startActivityForResult(intent, MainActivity.RETURN_TO_HOME);
-                return false;
-            }
-        });
-
     }
 
 
@@ -97,15 +77,11 @@ public abstract class TrackingListAdapter<T extends RealmResults> extends Recycl
      **/
 
     public class TrackingListViewHolderNew extends RecyclerView.ViewHolder {
-        LinearLayout linearTrackHolder;
         private ListItemTrackingBinding binding;
 
         public TrackingListViewHolderNew(ListItemTrackingBinding binding) {
             super(binding.getRoot());
-
             this.binding = binding;
-            linearTrackHolder = (LinearLayout) itemView.findViewById(R.id.track_list_item);
-          //  linearTrackHolder = (LinearLayout) itemView.findViewById(R.id.content_track_list);
         }
 
         public ListItemTrackingBinding getBinding() {
