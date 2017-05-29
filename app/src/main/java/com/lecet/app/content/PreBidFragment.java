26cebc6aa4c405db2c310a1bf1824e62f914a2fd
Noel Anonas <noel.anonas@domandtom.com>
2017-05-29 07:44:05 -0400

    Refactor codes for PreBidFragment and PostBidFragment classes of Project Near Me. Created a BidFragment class as a superclass for PreBidFragment and PostBidFragment classes. Placed all the common attributes and methods in the BidFragment class. Deleted all the unnecessary/duplicated codes in the PreBid and PostBid fragment classes.

--- app/src/main/java/com/lecet/app/content/PreBidFragment.java
+++ app/src/main/java/com/lecet/app/content/PreBidFragment.java
@@ -1,64 +1,24 @@
 package com.lecet.app.content;
 
-
-import android.databinding.DataBindingUtil;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import com.lecet.app.R;
-import com.lecet.app.adapters.BidProjectListRecyclerViewAdapter;
+
 import com.lecet.app.data.models.Project;
-import com.lecet.app.databinding.FragmentPrePostBidBinding;
-import com.lecet.app.viewmodel.SearchViewModel;
+
 import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 
-//TODO: make an inheritance for this fragment and all common attributes and methods for PreBid and postbid.
-public class PreBidFragment extends Fragment {
-    static private ArrayList<Project> bidData;
-
+//TODO: Make an inheritance for this fragment and all common attributes and methods for PreBid and postbid.-Done
+public class PreBidFragment extends BidFragment {
     public static PreBidFragment newInstance(ArrayList<Project> data) {
         PreBidFragment fragmentInstance = new PreBidFragment();
-        //  activity = ac;
         bidData = data;
         return fragmentInstance;
     }
 
     public PreBidFragment() {
         // Required empty public constructor
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = initDataBinding(inflater, container);
-
-        initAdapter(view);
-
-        return view;
-    }
-
-    private View initDataBinding(LayoutInflater inflater, ViewGroup container) {
-        FragmentPrePostBidBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_pre_post_bid, container, false);
-        return binding.getRoot();
-    }
-
-    void initAdapter(View view) {
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_prepostbid);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayout.VERTICAL, false);
-        recyclerView.setLayoutManager(layoutManager);
-        BidProjectListRecyclerViewAdapter searchAdapterProjectAll =
-                new BidProjectListRecyclerViewAdapter(getActivity(), SearchViewModel.SEARCH_ADAPTER_TYPE_PROJECT_QUERY_ALL, bidData);
-        //recyclerView.scrollToPosition(1);
-        recyclerView.setAdapter(searchAdapterProjectAll);
     }
 }
