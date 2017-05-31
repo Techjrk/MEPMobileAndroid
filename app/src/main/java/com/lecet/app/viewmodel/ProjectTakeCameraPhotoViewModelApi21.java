960532d9e9915ef10105c298ed248539eddc36e4
Ali Onque <ali@domandtom.com>
2017-05-31 14:55:03 -0400

    **Changes** Fixed, 9274: MPP 3.1: There’s a white space below the image after it is selected. Error when pausing device would freeze camera Error when selecting Library **Known Errors** None
    **Next Steps**
    Removing library edge cases

--- app/src/main/java/com/lecet/app/viewmodel/ProjectTakeCameraPhotoViewModelApi21.java
+++ app/src/main/java/com/lecet/app/viewmodel/ProjectTakeCameraPhotoViewModelApi21.java
@@ -67,7 +67,7 @@ public class ProjectTakeCameraPhotoViewModelApi21 extends BaseObservable {
     private final int MAX_IMAGE_SIZE = 700000;
     private final int REDUCED_IMAGE_AMT = 10;
 
-    private static List<String> rotatedCameraManufacturers = new ArrayList<String>(Arrays.asList("samsung"));//A list of all manufacturers that have non-standard camera implimentations. Used to decide camera rotation.
+    private static List<String> rotatedCameraManufacturers = new ArrayList<String>(Arrays.asList("samsung", "asus"));//A list of all manufacturers that have non-standard camera implimentations. Used to decide camera rotation.
     private static CameraPreview cameraPreview;
     private static SparseIntArray ORIENTATIONS;
 
