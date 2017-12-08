5262ac3c9870e8707077d97c42e82fcecd0a5b4b
Jason Mortara <jason.mortara@domandtom.com>
2017-12-08 18:10:59 -0500

    updates to Zendesk support. need correct credentials.

--- app/src/main/java/com/lecet/app/content/HelpActivity.java
+++ app/src/main/java/com/lecet/app/content/HelpActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 
 import com.lecet.app.R;
 import com.lecet.app.contentbase.LecetBaseActivity;
@@ -14,7 +15,12 @@ import com.lecet.app.data.api.LecetClient;
 import com.lecet.app.data.storage.LecetSharedPreferenceUtil;
 import com.lecet.app.databinding.ActivitySettingsBinding;
 import com.lecet.app.domain.UserDomain;
+import com.lecet.app.utility.Log;
 import com.lecet.app.viewmodel.SettingsViewModel;
+import com.zendesk.sdk.model.access.AnonymousIdentity;
+import com.zendesk.sdk.model.access.Identity;
+import com.zendesk.sdk.network.impl.ZendeskConfig;
+import com.zendesk.sdk.support.SupportActivity;
 
 import io.realm.Realm;
 
@@ -34,6 +40,20 @@ public class HelpActivity extends LecetBaseActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_help);
+
+        initZendesk();
+
+        Button mHelpButton;
+        mHelpButton = (Button) findViewById(R.id.help_button);
+        mHelpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showZendesk();
+            }
+        });
+
+        //showZendesk();
+
 
 //        ActivitySettingsBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_help);
 //        LecetSharedPreferenceUtil pref = LecetSharedPreferenceUtil.getInstance(this);
@@ -60,6 +80,24 @@ public class HelpActivity extends LecetBaseActivity {
             viewModel.setToolbar(tb, title, subTitle);
         }
     }*/
+
+
+    private void initZendesk() {
+        Log.d(TAG, "initZendesk");
+
+        //TODO: replace with Lecet Zendesk credentials
+        ZendeskConfig.INSTANCE.init(this, "https://omniwear.zendesk.com", "23705744c16d8e0698b45920f18aa26e43d7", "mobile_sdk_client_b7fd695c0e9a6056");
+
+        ZendeskConfig.INSTANCE.setCoppaEnabled(true);
+        Identity identity = new AnonymousIdentity.Builder().build();
+        ZendeskConfig.INSTANCE.setIdentity(identity);
+    }
+
+    private void showZendesk() {
+        Log.d(TAG, "showZendesk");
+        new SupportActivity.Builder().show(HelpActivity.this);
+    }
+
 
     @Override
     public void onNetworkConnectionChanged(boolean isConnected, NetworkInfo networkInfo) {
